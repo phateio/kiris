20512c24041744e31ddc1d988e377ee016d86505
JRS <jrsdotdung@users.noreply.github.com>
2016-02-20 16:00:12 +0800

    Fix Missing template default/index Rollbar#5

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  root 'default#index'
+  root 'default#index', defaults: { format: 'html' }
 
   get 'news'     => 'notices#index', format: false
   get 'search'   => 'search#index', format: false
