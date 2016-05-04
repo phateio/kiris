16f4eea14d8cd2425abeb008214496ec61dfb587
Shi-Ken Don <shikendon@users.noreply.github.com>
2016-05-05 01:26:10 +0800

    Refactor YP directories service object

--- app/controllers/concerns/yp_directory.rb
+++ app/controllers/concerns/yp_directory.rb
@@ -1,49 +1,36 @@
-require 'net/http'
+require 'zlib'
+require 'httparty'
 
 class YPDirectory
   attr_reader :yp_url
   attr_reader :yp_sid_key
   attr_reader :sid
 
-  def http_request(uri, req)
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.open_timeout = 1
-    http.read_timeout = 1
-    http.request(req)
-  end
-
   def initialize(yp_url)
-    yp_unique = Zlib::crc32(yp_url).to_s(16).rjust(8, '0')
+    yp_unique = Zlib.crc32(yp_url).to_s(16).rjust(8, '0')
     @yp_url = yp_url
     @yp_sid_key = "yp_#{yp_unique}_sid".to_sym
     @sid ||= Rails.cache.read(@yp_sid_key)
   end
 
-  def add(params)
-    params.merge!(action: 'add')
-    uri = URI.parse(@yp_url)
-    req = Net::HTTP::Post.new(uri.request_uri)
-    req.set_form_data(params)
-    res = http_request(uri, req)
-    @sid = res['SID']
+  def add(params = {})
+    params[:action] = 'add'
+    response = HTTParty.post(@yp_url, query: params)
+    @sid = response.headers['SID']
     Rails.cache.write(@yp_sid_key, @sid)
   end
 
-  def touch(params)
-    params.merge!(action: 'touch', sid: @sid)
-    uri = URI.parse(@yp_url)
-    req = Net::HTTP::Post.new(uri.request_uri)
-    req.set_form_data(params)
-    res = http_request(uri, req)
-    reset if res['YPMessage'].include?('SID does not exist')
+  def touch(params = {})
+    params[:action] = 'touch'
+    params[:sid] = @sid
+    response = HTTParty.post(@yp_url, query: params)
+    reset if response.headers['YPMessage'].include?('SID does not exist')
   end
 
-  def remove
-    params.merge!(action: 'remove', sid: @sid)
-    uri = URI.parse(@yp_url)
-    req = Net::HTTP::Post.new(uri.request_uri)
-    req.set_form_data(params)
-    res = http_request(uri, req)
+  def remove(params = {})
+    params[:action] = 'remove'
+    params[:sid] = @sid
+    HTTParty.post(@yp_url, query: params)
     reset
   end
 
