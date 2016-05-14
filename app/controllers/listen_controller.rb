3cc56a6f9e0722ad4b3c48f2406e2f376c1e233b
Shi-Ken Don <shikendon@users.noreply.github.com>
2016-05-14 14:50:33 +0800

    Revert "Use cdnstream.phate.io for https listener"
    This reverts commit 66c1a823dde4b96af0d9e57f20af0202fd1c96c4.

--- app/controllers/listen_controller.rb
+++ app/controllers/listen_controller.rb
@@ -2,21 +2,21 @@ class ListenController < ApplicationController
   layout false
 
   def redirect
-    @icy_metadata = request.env['HTTP_ICY_METADATA']
-    stream_protocol = request.protocol # Returns 'https://' if this is an SSL request and 'http://' otherwise.
+    icy_metadata = request.env['HTTP_ICY_METADATA']
+    request_protocol = request.protocol # Returns 'https://' if this is an SSL request and 'http://' otherwise.
 
     if $ICECAST_SERVER
       stream_server = $ICECAST_SERVER
-    elsif request.host != default_url_options.to_h[:host] || stream_protocol == 'https://'
+    elsif request.host != default_url_options.to_h[:host]
       stream_server = 'cdnstream.phate.io'
-    # elsif @icy_metadata.to_s == '1' || @useragent.to_s.downcase.include?('mobile')
+    # elsif icy_metadata.to_s == '1' || @useragent.to_s.downcase.include?('mobile')
     #   stream_server = 'stream.dallas.phate.io'
     else
       stream_server = 'stream.phate.io'
     end
 
     query_string = request.query_string
-    location = "#{stream_protocol}#{stream_server}/phatecc"
+    location = "#{request_protocol}#{stream_server}/phatecc"
     location += '.mp3' if params[:format] == 'mp3'
     location += "?#{query_string}" if query_string.length > 0
     redirect_to location
