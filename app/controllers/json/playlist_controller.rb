d3769d1fb5627598c012aed973632141daf6d7e9
Shi-Ken Don <shikendon@users.noreply.github.com>
2016-08-09 23:58:13 +0800

    Revert "Disable automatic changing background temporarily"
    This reverts commit 677d450a203763243340abe02ba225e6ea0c53ea.

--- app/controllers/json/playlist_controller.rb
+++ app/controllers/json/playlist_controller.rb
@@ -20,7 +20,7 @@ class Json::PlaylistController < ApplicationController
             images = track.images.order(id: :asc).select { |image| (/\ARANK_(?:2|3|4|5)\z/ =~ image.status) != nil }
             image = images.present? ? images.at(playlist.id % images.size) : nil
 
-            if false && image
+            if image
               image_info = {
                 url: image.cdn_url,
                 source: image.source,
