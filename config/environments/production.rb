eaae355764481ced6e2fa99908a19a347dcb4226
Shi-Ken Don <shikendon@users.noreply.github.com>
2016-08-21 11:44:33 +0800

    Add X-Frame-Options to default headers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,6 +42,10 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
+  config.action_dispatch.default_headers = {
+    'X-Frame-Options' => 'ALLOWALL' # 'SAMEORIGIN', 'DENY', 'ALLOWALL'
+  }
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
