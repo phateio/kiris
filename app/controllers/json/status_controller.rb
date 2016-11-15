7156605156706440b08dc57e85fddb302e7cee5b
Shi-Ken Don <shikendon@users.noreply.github.com>
2016-11-15 08:56:40 +0800

    Improve status controller performance

--- app/controllers/json/status_controller.rb
+++ app/controllers/json/status_controller.rb
@@ -6,22 +6,12 @@ class Json::StatusController < ApplicationController
   def index
     items = {}
 
-    CacheLock.synchronize(:status) do
-      items = Rails.cache.fetch(:status, expires_in: 60.seconds) do
-        total_listeners = 0
-        servers = $ICECAST_RELAYS.to_s.split(',')
-        servers.each do |server|
-          res_body = http_get_response_body("http://#{server}/status-json.xsl") rescue nil
-          next if res_body.nil?
-          icecast_status = JSON.parse(res_body)
-          icecast_sources = icecast_status['icestats']['source']
-          listeners = Array(icecast_sources).map { |source| source['listeners'] }.inject(0, :+)
-          total_listeners += listeners
-        end
-        items[:date] = Time.now.utc
-        items[:listeners] = total_listeners
-        items
-      end
+    icecast_status = Rails.cache.read('icecast_status')
+    if icecast_status
+      icecast_sources = icecast_status['icestats']['source']
+      listeners = Array(icecast_sources).map { |source| source['listeners'] }.inject(0, :+)
+      items[:date] = Time.now.utc
+      items[:listeners] = listeners
     end
 
     respond_to do |format|
