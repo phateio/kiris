34196963be17630cbf421b74961e1562aa7a2768
Shi-Ken Don <shiken.don@gmail.com>
2017-05-30 05:04:41 +0800

    Fix condition of recent histories of random track list

--- app/controllers/bridge/playlist_controller.rb
+++ app/controllers/bridge/playlist_controller.rb
@@ -79,8 +79,9 @@ class Bridge::PlaylistController < ApplicationController
 
   def randlist
     items = []
-    played_tracks = Track.joins(:histories).where(History.arel_table[:playedtime].gt(7.days.ago))
-    available_tracks = Track.where.not(id: played_tracks).requestable.utaitedb.implicit.utattemita
+    recent_histories = History.arel_table[:playedtime].gt(7.days.ago)
+    played_tracks = Track.joins(:histories).where(recent_histories).select(:title).distinct
+    available_tracks = Track.where.not(title: played_tracks).requestable.utaitedb.implicit.utattemita
     track_size = available_tracks.size
     offset = track_size >= 2000 ? rand(2000) : rand(track_size)
     tracks = available_tracks.order(count: :desc).offset(offset).limit(1)
